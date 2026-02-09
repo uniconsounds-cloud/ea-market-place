cfd76edc8b4c43304ac753f99113ff1dc2a4bcfd
Suphakorn Juntarasate <suphakorn@MacBookAir.lan>
2026-02-08 20:36:36 -0800

    fix: typo in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(request: NextRequest) {
         },
     })
 
-    constsupabase = createServerClient(
+    const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
